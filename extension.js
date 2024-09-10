8de428b005d33b6f9f66063b66137daef3ca6a6f
IGS <amaanulhaq55@gmail.com>
2024-09-10 15:11:35 +0500

    extension updated

--- extension.js
+++ extension.js
@@ -24,29 +24,3 @@ module.exports = {
   activate,
   deactivate
 };
-
-
-
-
-// const vscode = require('vscode');
-// const scaffoldProject = require('./src/Init-Project');
-
-// function activate(context) {
-//   console.log('CodeJumpstart: Fuel your projects, one scaffold at a time.');
-
-//   let scaffoldCommand = vscode.commands.registerCommand('codejumpstart.scaffoldProject', async function () {
-//     vscode.window.showInformationMessage('Kickstarting the code generator... Amaan is on the job!');
-
-//     // Call the scaffoldProject function here
-//     await scaffoldProject();
-//   });
-
-//   context.subscriptions.push(scaffoldCommand);
-// }
-
-// function deactivate() {}
-
-// module.exports = {
-//   activate,
-//   deactivate
-// };
