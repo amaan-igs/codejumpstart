75333af6088107fd80a9baa244776524ee6f226f
IGS <amaanulhaq55@gmail.com>
2024-09-10 14:32:51 +0500

    VS Maj Update ++

--- extension.js
+++ extension.js
@@ -1,17 +1,21 @@
 const vscode = require('vscode');
-const scaffoldProject = require('./src/scaffoldProject');
+const initializeProject = require('./src/Init-Project');
 
 function activate(context) {
-  console.log('CodeJumpstart: Fuel your projects, one scaffold at a time.');
+  console.log('CodeJumpstart: Fuel your projects, one setup at a time.');
 
-  let scaffoldCommand = vscode.commands.registerCommand('codejumpstart.scaffoldProject', async function () {
+  // Register the initializeProject command
+  let initializeCommand = vscode.commands.registerCommand('codejumpstart.initializeProject', async function () {
     vscode.window.showInformationMessage('Kickstarting the code generator... Amaan is on the job!');
 
-    // Call the scaffoldProject function here
-    await scaffoldProject();
+    // Call the initializeProject function here
+    await initializeProject();
+
+    // Show success message when initialization is complete
+    vscode.window.showInformationMessage('All set! Build something epic.');
   });
 
-  context.subscriptions.push(scaffoldCommand);
+  context.subscriptions.push(initializeCommand);
 }
 
 function deactivate() {}
@@ -20,3 +24,29 @@ module.exports = {
   activate,
   deactivate
 };
+
+
+
+
+// const vscode = require('vscode');
+// const scaffoldProject = require('./src/Init-Project');
+
+// function activate(context) {
+//   console.log('CodeJumpstart: Fuel your projects, one scaffold at a time.');
+
+//   let scaffoldCommand = vscode.commands.registerCommand('codejumpstart.scaffoldProject', async function () {
+//     vscode.window.showInformationMessage('Kickstarting the code generator... Amaan is on the job!');
+
+//     // Call the scaffoldProject function here
+//     await scaffoldProject();
+//   });
+
+//   context.subscriptions.push(scaffoldCommand);
+// }
+
+// function deactivate() {}
+
+// module.exports = {
+//   activate,
+//   deactivate
+// };
